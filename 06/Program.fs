c9897bd7f4d1f0b94f8c3764015f60cdb17a75b2
Ralph Hendriks <r.hendriks@tjip.com>
2019-12-06 21:52:12 +0100

    Day 6

---
+++ 06/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main _ =
+    let directOrbits = 
+        File.ReadAllLines("input.txt")
+        |> Seq.map (fun line -> line.Split(')') |> function [|a; b|] -> b, a | _ -> failwith "Invalid array length")
+        |> Map.ofSeq
+
+    let rec allOrbits = function
+    | "COM" -> []
+    | x -> directOrbits.[x] :: (allOrbits directOrbits.[x])
+
+    let totalOrbits =
+        directOrbits
+        |> Map.toSeq
+        |> Seq.map (fun (k, _) -> allOrbits k |> List.length)
+        |> Seq.sum
+
+    printfn "Answer 1: %i" totalOrbits
+
+    let allSanOrbits = allOrbits "SAN"
+    let allYouOrbits = allOrbits "YOU"
+
+    let rec orbitDistance findIndex = function
+    | [] -> failwith "No path found"
+    | v::vs ->
+        match (List.tryFindIndex (fun x -> x = v) allSanOrbits) with
+        | None -> orbitDistance (findIndex + 1) vs
+        | Some dist -> findIndex + dist
+
+    printfn "Answer 2: %i" (orbitDistance 0 allYouOrbits)
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
